e8cfad71b26b4fb30193fac9cc8e94856281530c
Анастасия Полякова <78700473+Poliakkova@users.noreply.github.com>
2024-09-30 00:48:19 +0300

    Виводимо усі доручення з бд

--- frontend/src/components/ListInstructionsComponent.jsx
+++ frontend/src/components/ListInstructionsComponent.jsx
@@ -30,42 +30,6 @@ const ListInstructionsComponent = () => {
       };
 
   return (
-    // <div className='container'>
-    //     <h2 className='text-center'>Усі доручення</h2>
-    //     <button className='btn btn-primary mb-2' onClick={createInstruction}>Створити доручення</button>
-    //     <table className='table table-striped table-bordered'>
-    //         <thead>
-    //             <tr>
-    //                 <td>id</td>
-    //                 <td>title</td>
-    //                 <td>head</td>
-    //                 <td>headControl</td>
-    //                 <td>status</td>
-    //                 <td>sourceOfInstruction</td>
-    //                 <td>shortDescription</td>
-    //                 <td>startTime</td>
-    //                 <td>expTime</td>
-    //                 <td></td>
-    //             </tr>
-    //         </thead>
-    //         <tbody>
-    //             {
-    //                 instructions.map(instruction => 
-    //                 <tr key={instruction.id}>
-    //                     <td>{instruction.id}</td>
-    //                     <td>{instruction.title}</td>
-    //                     <td>{instruction.headSurname} {instruction.headName} {instruction.headPatronymic}</td>
-    //                     <td>{instruction.headControl} {instruction.headControlName} {instruction.headControlPatronymic}</td>
-    //                     <td>{instruction.status}</td>
-    //                     <td>{instruction.sourceOfInstruction}</td>
-    //                     <td>{instruction.shortDescription}</td>
-    //                     <td>{instruction.startTime}</td>
-    //                     <td>{instruction.expTime}</td>
-    //                 </tr>)
-    //             }
-    //         </tbody>
-    //     </table>
-    // </div>
     <body>
     <div className="wrapper">
         <div className="sidebar">
@@ -84,21 +48,39 @@ const ListInstructionsComponent = () => {
                     <thead>
                         <tr>
                             <th><input type='checkbox'/></th>
-                            <th onclick={() => sortTable(0)}>Дата</th>
-                            <th onclick={() => sortTable(1)}>Відповідальний</th>
-                            <th onclick={() => sortTable(2)}>Керуючий процесом</th>
-                            <th onclick={() => sortTable(3)}>Джерело</th>
-                            <th onclick={() => sortTable(3)}>Тип</th>
-                            <th onclick={() => sortTable(4)}>Назва</th>
-                            <th onclick={() => sortTable(5)}>Опис</th>
-                            <th onclick={() => sortTable(6)}>Початок</th>
-                            <th onclick={() => sortTable(7)}>Дедлайн</th>
-                            <th onclick={() => sortTable(8)}>Статус</th>
-                            <th onclick={() => sortTable(8)}></th>
-                            <th onclick={() => sortTable(8)}></th>
+                            <th>Дата</th>
+                            <th>Відповідальний</th>
+                            <th>Керуючий процесом</th>
+                            <th>Джерело</th>
+                            <th>Тип</th>
+                            <th>Назва</th>
+                            <th>Опис</th>
+                            <th>Початок</th>
+                            <th>Дедлайн</th>
+                            <th>Статус</th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            instructions.map(instruction => 
+                                <tr key={instruction.id}>
+                                    <td><input type='checkbox'/></td>
+                                    <td>11.09.24</td>
+                                    <td>{instruction.headSurname} {instruction.headName} {instruction.headPatronymic}</td>
+                                    <td>{instruction.headControl} {instruction.headControlName} {instruction.headControlPatronymic}</td>
+                                    <td>{instruction.sourceOfInstruction}</td>
+                                    <td>Науково-методична робота</td>
+                                    <td>{instruction.title}</td>
+                                    <td>{instruction.shortDescription}</td>
+                                    <td>{new Date(instruction.startTime).toLocaleDateString()}</td>
+                                    <td>{new Date(instruction.expTime).toLocaleDateString()}</td>
+                                    <td><span className="status orange">{instruction.status}</span></td>
+                                    <td><i className="bi bi-pencil-square" style={{ fontSize: '18px'}}></i></td>
+                                    <td><i className="bi bi-trash3" style={{ fontSize: '18px'}}></i></td>
+                                </tr>)
+                        }
                         <tr onClick={() => handleRowClick(1)} style={{ cursor: 'pointer' }}>
                             <td><input type='checkbox'/></td>
                             <td>11.09.24</td>
